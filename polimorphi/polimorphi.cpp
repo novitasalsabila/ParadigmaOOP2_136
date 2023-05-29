0539671467bfa2b4d8947d0dfa4bbbb3e710ab02
novitasalsabila <novnovv10@gmail.com>
2023-05-29 19:56:19 +0700

    Membuat class joko yaitu turunan dari class seseorang dan mengimplementasikan method pesan() untuk mencetak pesan dari joko

--- polimorphi/polimorphi.cpp
+++ polimorphi/polimorphi.cpp
@@ -9,3 +9,10 @@ public:
     //cout <<"pesan dari seseorang" <<endl;
     //}
 };
+
+class joko :public seseorang {
+public:
+    void pesan() {
+        cout << "pesan dari joko" << endl;
+    }
+};
