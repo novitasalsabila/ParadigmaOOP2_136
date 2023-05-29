e3647c5f6e4856697fab0ab620595ae7b6202f25
novitasalsabila <novnovv10@gmail.com>
2023-05-29 21:04:07 +0700

    Membuat class AbstraksiKlas yang memiliki dua atribut x dan y yang bertipe string dan memiliki visibilitas private berarti atribut-atribut ini hanya dapat diakses oleh metode-metode dalam kelas itu sendiri. Memiliki akses public yang memiliki member method setXY dan method display untuk menampilkan

---
+++ abstraksiKls/abstraksiKls.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class AbstraksiKlas {
+private: string x, y;
+
+public:
+
+    // method untuk mengisi nilai  
+    // private member
+    void setXY(string a, string b) { //digunakan untuk mengisi nilai dari atribut x dan y yang berada dalam kelas.
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai  
+    void display() { //digunakan untuk menampilkan nilai dari atribut x dan y ke output menggunakan cout
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
