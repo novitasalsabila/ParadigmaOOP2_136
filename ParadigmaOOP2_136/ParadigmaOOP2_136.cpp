85a5e024b01a0f3cf1120d85e6eee6331857a2b2
novitasalsabila <novnovv10@gmail.com>
2023-05-29 11:00:58 +0700

    Membuat class pekerja yang merupakan turunan dari class orang

--- ParadigmaOOP2_136/ParadigmaOOP2_136.cpp
+++ ParadigmaOOP2_136/ParadigmaOOP2_136.cpp
@@ -12,3 +12,14 @@ public:
 		cout << "Orang dibuat dengan umur" << umur << "\n" << endl;
 	}
 };
+
+
+class pekerja :public orang {
+public:
+	pekerja(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pekerja dibuat\n" << endl;
+	}
+};
+
