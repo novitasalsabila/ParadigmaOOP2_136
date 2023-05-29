df4efc8d5782a341ab4aa82ed31bb826dd5f85b7
novitasalsabila <novnovv10@gmail.com>
2023-05-29 19:58:18 +0700

    Membuat class lia yaitu turunan dari class seseorang dan mengimplementasikan method pesan() untuk mencetak pesan dari lia

--- polimorphi/polimorphi.cpp
+++ polimorphi/polimorphi.cpp
@@ -16,3 +16,12 @@ public:
         cout << "pesan dari joko" << endl;
     }
 };
+
+
+class lia : public seseorang {
+public:
+    void pesan() {
+        cout << "pesan dari lia" << endl;
+    }
+};
+
